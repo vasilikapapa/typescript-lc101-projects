d545ddda863cc16fa684a379347ebffa396a89cb
vasilikapapa <vasilikapp@gmail.com>
2022-07-27 15:58:18 -0500

    Studio Completed

---
+++ studio/Astronaut.ts
@@ -0,0 +1,13 @@
+import { Payload } from './Payload';
+
+
+export class Astronaut implements Payload {
+    // properties and methods
+    massKg: number;
+    name: string;
+
+    constructor(massKg: number, name: string) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+ }
---
+++ studio/Rocket.ts
@@ -0,0 +1,54 @@
+import { Cargo } from "./Cargo";
+import { Astronaut } from "./Astronaut";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    
+    sumMass(items: Payload[]): number {
+        let totalMassKg: number = 0;
+        for (let i=0; i<items.length; i++) {
+            totalMassKg += items[i].massKg;
+        }
+        return totalMassKg;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+ }
